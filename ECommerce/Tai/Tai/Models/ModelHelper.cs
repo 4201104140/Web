0495edd5189ea95b70cbc9476ecb6c78c1fb5ab2
Phan Tan Tai <phantantai9800@gmail.com>
2019-10-18 07:46:16 +0700

    tai

---
+++ ECommerce/Tai/Tai/Models/ModelHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Tai.Models
+{
+    public static class ModelHelper
+    {
+        public static string ToUrlFriendly(this string stri)
+        {
+            string str = stri.ToLower().Trim();
+
+            str = Regex.Replace(str, @"[aáàảãạâấầẩẫậăắằẳẵặ]", "a");
+            str = Regex.Replace(str, @"[đ]", "d");
+            str = Regex.Replace(str, @"[eéèẻẽẹêếềểễệ]", "e");
+            str = Regex.Replace(str, @"[iíìỉĩị]", "i");
+            str = Regex.Replace(str, @"[oóòỏõọôốồổỗộơớờởỡợ]", "o");
+            str = Regex.Replace(str, @"[uúùủũụưứừửữự]", "u");
+            str = Regex.Replace(str, @"\s+", "-").Trim();
+            str = Regex.Replace(str, @"\s", "-");
+
+            return str;
+        }
+    }
+}
