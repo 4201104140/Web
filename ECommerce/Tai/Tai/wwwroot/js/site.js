4baf9df089c52372af9558da1214467b584c1593
Phan Tan Tai <phantantai9800@gmail.com>
2019-11-17 21:55:49 +0700

    Cart

--- ECommerce/Tai/Tai/wwwroot/js/site.js
+++ ECommerce/Tai/Tai/wwwroot/js/site.js
@@ -2,3 +2,28 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+(function () {
+
+    $("#cart").on("click", function () {
+        debugger;
+        $(".shopping-cart").fadeToggle("fast");
+    });
+    
+})();
+function AddToCart(product, qty = 1) {
+    var data = {
+        "Product": {
+            "ProductId": product
+        },
+        "Quantity": qty
+    };
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            location.reload();
+        }
+    };
+    xhttp.open("POST", "/Cart/AddToCart");
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhttp.send(JSON.stringify(data));
+}
